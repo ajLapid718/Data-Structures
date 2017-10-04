9d57ae5b258f43f5a7fc8c606bfb60586d3e24b5
Allan James Lapid <ajLapid718@gmail.com>
2017-10-03 23:52:22 -0400

    Rename files && add stack implementation

---
+++ Personal-Notes/Stack/Implementation_Via_Aja_Hammerly.rb
@@ -0,0 +1,48 @@
+class Stack
+  def initialize
+    @data = []
+  end
+
+  def push(a)
+    @data.push(a)
+  end
+
+  def pop
+    @data.pop
+  end
+
+  def empty?
+    @data.empty?
+  end
+
+  def display
+    puts "top"
+    puts @data
+    puts "bottom"
+  end
+end
+
+# "If you don’t want to use the built in methods here is another implementation that keeps track of the index of the last item put on the stack."
+
+class Stack
+  def initialize
+    @data = []
+    @head = -1
+  end
+
+  def push(a)
+    @data << a
+    @head += 1
+  end
+
+  def pop
+    result = @data[@head]
+    @data.delete_at(@head)
+    @head -= 1
+    result
+  end
+
+  def empty?
+    @head == -1
+  end
+end
