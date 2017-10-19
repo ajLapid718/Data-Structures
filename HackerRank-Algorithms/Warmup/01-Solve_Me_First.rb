bb89360683ead5120ff02c64fabdd68d130ddb9a
Allan James Lapid <ajLapid718@gmail.com>
2017-10-18 23:28:48 -0400

    Add files

---
+++ HackerRank-Algorithms/Warmup/01-Solve_Me_First.rb
@@ -0,0 +1,21 @@
+Link: https://www.hackerrank.com/challenges/solve-me-first/problem
+
+Task
+Review the code provided in the editor below, then complete the solveMeFirst function so that it returns the sum of two integers read from stdin.
+
+Input Format
+Code that reads input from stdin is provided for you in the editor. There are  lines of input, and each line contains a single integer.
+
+Output Format
+Code that prints the sum calculated and returned by solveMeFirst is provided for you in the editor.
+
+# My solution
+def solveMeFirst (a, b)
+    a + b
+end
+
+# Provided by HackerRank
+val1 = gets.to_i
+val2 = gets.to_i
+sum = solveMeFirst(val1,val2)
+print (sum)
