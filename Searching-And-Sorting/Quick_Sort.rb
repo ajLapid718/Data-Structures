cc5b9f09b122e6f12b5d93004443febc2fd4c5c2
Allan James Lapid <ajLapid718@gmail.com>
2017-10-21 03:46:45 -0400

    Define quicksort

--- Searching-And-Sorting/Quick_Sort.rb
+++ Searching-And-Sorting/Quick_Sort.rb
@@ -1,1 +1,6 @@
-Placeholder
+def quicksort(arr)
+  return [] if arr.empty?
+  pivot_element = arr.first
+  left_of_pivot, right_of_pivot = arr[1..-1].partition { |num| num <= pivot_element }
+  quicksort(left_of_pivot) + [pivot_element] + quicksort(right_of_pivot)
+end
