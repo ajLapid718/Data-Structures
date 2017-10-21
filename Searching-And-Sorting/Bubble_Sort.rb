855770da1e0315c328a12cbd2093eedbebae7f36
Allan James Lapid <ajLapid718@gmail.com>
2017-10-21 04:06:21 -0400

    Define bubble sort

--- Searching-And-Sorting/Bubble_Sort.rb
+++ Searching-And-Sorting/Bubble_Sort.rb
@@ -1,1 +1,16 @@
-Placeholder
+def bubble_sort(arr)
+  loop do
+		flag = false
+
+		arr[0...-1].each_index do |i|
+			if arr[i] > arr[i+1]
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				flag = true
+			end
+		end
+
+		break if flag == false
+	end
+
+	arr
+end
