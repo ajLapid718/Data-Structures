d3cbb13e1504e37e1ab54b466c1424b1802a175f
Allan James Lapid <ajLapid718@gmail.com>
2018-04-08 17:29:18 -0400

    Write binary search iteratively

---
+++ Searching-And-Sorting/binarySearch.js
@@ -0,0 +1,21 @@
+function binarySearch(sortedArr, val) {
+  let leftIndex = 0;
+  let rightIndex = sortedArr.length - 1;
+
+  while (leftIndex < rightIndex) {
+    let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+    let element = arr[midIndex];
+
+    if (element === val) {
+      return midIndex;
+    }
+    else if (val < element) {
+      rightIndex = midIndex - 1;
+    }
+    else {
+      leftIndex = midIndex + 1;
+    }
+  }
+
+  return -1;
+}
