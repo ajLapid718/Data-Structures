2a0ac6326a84542a43d9b73880049301c9a32214
Allan James Lapid <ajLapid718@gmail.com>
2018-04-18 21:10:43 -0400

    Tabs and spaces are hard

--- Searching-And-Sorting/mergeSort.js
+++ Searching-And-Sorting/mergeSort.js
@@ -7,23 +7,23 @@ function mergeSort(array) {
   let left = array.slice(0, mid);
   let right = array.slice(mid);
 
-	return merge(mergeSort(left), mergeSort(right));
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
   let sorted = [];
 
-	let leftIdx = 0;
+  let leftIdx = 0;
   let rightIdx = 0;
 
   while (leftIdx < left.length && rightIdx < right.length) {
     if (left[leftIdx] < right[rightIdx]) {
-	    sorted.push(left[leftIdx]);
+      sorted.push(left[leftIdx]);
       leftIdx += 1;
     }
     else {
       sorted.push(right[rightIdx]);
-			rightIdx += 1;
+      rightIdx += 1;
     }
   }
 
